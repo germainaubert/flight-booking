33b1b7cd7986f93a2ed7e75b4cf76934b0e8c9ee
Arthur B <arthur.b@thewalkingnerds.com>
2023-09-07 13:31:50 +0200

    feat: front base + tailwind

---
+++ front/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="text-xl text-white text-center bg-black h-screen flex items-center justify-center">
+    Salut c le front ça marche bien ou quoi lesgo
+  </div>
+</template>
