af307b5159f900085d0a4f1d9c5b82b5017679c3
Arthur B <arthur.b@thewalkingnerds.com>
2023-09-07 14:49:36 +0200

    feat: finish front

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
+import Home from "./pages/Home.vue"
 </script>
 
 <template>
-  <div class="text-xl text-white text-center bg-black h-screen flex items-center justify-center">
-    Salut c le front ça marche bien ou quoii lesgo
-  </div>
+    <Home />
 </template>
---
+++ front/src/pages/Home.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Form from "./Form.vue";
+import flights from "../../../flights.json";
+import { ref } from "vue";
+const flightSelected = ref(null);
+
+const onClick = (flight) => {
+  flightSelected.value = flight;
+};
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <span>Choisissez un vol</span>
+    <div v-for="flight in flights" :key="flight.id">
+      <div class="flex flex-row border-2 border-gray-200 p-2 space-x-2">
+        <div class="flex flex-col">
+          <span> Départ : {{ flight.depart }}</span>
+          <span> Arrivée : {{ flight.arrival }}</span>
+        </div>
+        <button @click="onClick(flight)" class="border-2 border-gray-200 p-2">
+          Select
+        </button>
+      </div>
+      <div v-if="flightSelected">
+        <Form :flightSelected="flightSelected" />
+      </div>
+    </div>
+  </div>
+</template>
