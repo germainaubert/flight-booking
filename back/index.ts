5391122cc549453a058ead5e27d78a7f0ff42e94
gaubert <germain40k@hotmail.fr>
2023-09-08 16:30:03 +0200

    add: convertor service

--- back/index.ts
+++ back/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import { booking } from './src/routes/booking'
 import { flights } from './src/routes/flights'
-import { fetchConversionData } from './src/services/convertor'
+import fetchConversionData from './src/services/convertor'
 
 const app = express()
 const port = 3000
@@ -13,7 +13,7 @@ app.use(bodyParser.json())
 app.use('/booking', booking)
 app.use('/flights', flights)
 
-fetchConversionData();
+fetchConversionData(0);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- back/src/services/convertor.ts
+++ back/src/services/convertor.ts
@@ -1,5 +1,51 @@
-const url: string = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml';
+import axios from 'axios';
+import fs from 'fs';
 
-export const fetchConversionData = () => {
-    console.log("test");
+const url: string = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml';
+const maxAttempt = 5;
+
+function fetchConversionData (attempt: number): void {
+    attempt++;
+    axios.get(url)
+    .then(function (response) {
+        handleResponse(response.data);
+    })
+    .catch(function (error) {
+        console.log(error);
+        if(attempt < maxAttempt) {
+            console.log('Attempt n°'+attempt+' failed. Attempting in 5 minutes');
+            setTimeout(() => fetchConversionData(attempt), 300000);    
+        }
+    })
 }
+
+function handleResponse(data: string) {
+    const currencies = parseData(data);
+    if(currencies) {
+        writeConversionJSON(currencies);
+    } else {
+        console.log('Xml conversion object is empty');
+    }
+}
+
+function parseData(responseData: string): Object[] {
+    const currencies = [];
+    for(const res of responseData.matchAll(/currency='(\w+)' rate='([\d+.]+)'/gm)) {
+        currencies.push({currency: res[1], rate: parseFloat(res[2])});
+    }
+    return currencies;
+}
+
+function writeConversionJSON(currencies: Object[]): void {
+    fs.writeFile('./data/conversions.json', JSON.stringify(currencies), (e) => {
+        if(e) {
+            console.log("Unable to write conversion file");
+            console.log(e);
+        } else {
+            console.log('File has been saved');
+
+        }
+    });
+}
+
+export default fetchConversionData;
