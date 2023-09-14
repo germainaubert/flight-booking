497ec33bdd546c3bd7cbb84feb3b47d5c0ee7f78
Abdallah <abdallah@milango.io>
2023-09-14 14:31:22 +0200

    Restructuration des différents services (api interne, api externe)

--- back/index.ts
+++ back/index.ts
@@ -1,30 +1,8 @@
-import express from 'express'
-import { booking } from './src/routes/booking'
-import { flights } from './src/routes/flights'
-import { conversion } from './src/routes/conversion'
 import { fetchConversionData } from './src/services/convertor'
-import bodyParser from 'body-parser'
-import { handledError } from './src/contract'
-
-const app = express()
-const port = 3000
-const cors = require('cors')
-
-app.use(cors());
-app.use(bodyParser.json())
-
-app.use('/booking', booking)
-app.use('/flights', flights)
-app.use('/conversion', conversion)
-
-app.use((err: any, req: any, res: any, next: any) => {//middleware de gestion d'erreur
-  const handledError: handledError = err
-  res.status(handledError.code).send(handledError.message)
-})
+import { launchExternalApi } from './src/externalApi'
+import { launchInternalApi } from './src/internalApi'
 
 fetchConversionData(0);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+launchInternalApi()
+launchExternalApi()
