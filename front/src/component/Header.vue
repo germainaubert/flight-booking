3ea87b7adf00116935b9e98899a9258673b08bf1
LEO GUIMARD <leo.g@live.com>
2023-09-14 10:55:59 +0200

    Remove ConsoleLog

--- front/src/component/Header.vue
+++ front/src/component/Header.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref } from 'vue';
 
-const emits = defineEmits(["searchBooking", "getFlightsByDate"]);
+const emits = defineEmits(['searchBooking', 'getFlightsByDate']);
 
 const date = ref();
 const bookingSearch = ref();
 
 const searchBooking = () => {
-  emits("searchBooking", bookingSearch.value);
+  emits('searchBooking', bookingSearch.value);
 };
 
 const getFlightsByDate = () => {
-  console.log(date.value);
-  emits("getFlightsByDate", date.value);
+  emits('getFlightsByDate', date.value);
 };
 </script>
 
@@ -20,9 +19,7 @@ const getFlightsByDate = () => {
   <header>
     <div class="flex flex-row justify-between items-center p-4">
       <div>
-        <label
-          for="text"
-          class="block text-sm font-medium leading-6 text-gray-900 pb-1"
+        <label for="text" class="block text-sm font-medium leading-6 text-gray-900 pb-1"
           >Search your booking</label
         >
         <input
@@ -37,7 +34,12 @@ const getFlightsByDate = () => {
         <h1 class="text-4xl font-bold">Flight booking</h1>
       </div>
       <div>
-        <input type="date" v-model="date" @change="getFlightsByDate" class="block rounded-md border  border-black p-2 text-gray-900 shadow-sm" />
+        <input
+          type="date"
+          v-model="date"
+          @change="getFlightsByDate"
+          class="block rounded-md border border-black p-2 text-gray-900 shadow-sm"
+        />
       </div>
     </div>
   </header>
