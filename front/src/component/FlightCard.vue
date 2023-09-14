98969019212a0cd3585718bd34f0140eee68952e
Arthur B <arthur.b@thewalkingnerds.com>
2023-09-14 16:41:14 +0200

    t'as capté ffallait traduire le bail mdr abonne toi @reussitenotoriete

--- front/src/component/FlightCard.vue
+++ front/src/component/FlightCard.vue
@@ -51,7 +51,7 @@ const redirectBooking = (flight: Flight) => {
           </p>
           
           <p class="text-gray-700 text-base" v-if="flightRef?.route?.length >= 3">
-            Escales : {{ flightRef?.route?.slice(1, -1).join("-") }}
+            Stops : {{ flightRef?.route?.slice(1, -1).join("-") }}
           </p>
         </div>
         <div class="flex flex-row justify-between">
@@ -59,7 +59,7 @@ const redirectBooking = (flight: Flight) => {
             <dl class="mx-auto grid grid-cols-1 gap-px bg-gray-900/5 lg:grid-cols-3">
               <div class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8">
                 <dt class="text-sm font-medium leading-6 text-gray-500">
-                  Prix du vol
+                  Fly cost
                 </dt>
                 <dd class="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">
                   {{ applyReduction(flightRef, 0.1, ROUTE) }}€
@@ -67,7 +67,7 @@ const redirectBooking = (flight: Flight) => {
               </div>
               <div class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8">
                 <dt class="text-sm font-medium leading-6 text-gray-500">
-                  Option végétarien
+                  Vegetarian menu 
                 </dt>
                 <dd class="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">
                  <!-- if flightref.menuVege icon check or close -->
@@ -85,7 +85,7 @@ const redirectBooking = (flight: Flight) => {
                       d="M5 13l4 4L19 7"
                       />
                   </svg>
-                  <span class="text-sm">Inclus</span>
+                  <span class="text-sm">Included</span>
                 </div>
                 <div v-else class="text-red-500 flex items-center space-x-2">
                   <svg
@@ -101,7 +101,7 @@ const redirectBooking = (flight: Flight) => {
                       d="M6 18L18 6M6 6l12 12"
                       />
                   </svg>
-                  <span class="text-sm">Non inclus</span>
+                  <span class="text-sm">Non Included</span>
                 </div>
                 </dd>
                 </div>
@@ -109,7 +109,7 @@ const redirectBooking = (flight: Flight) => {
               <div class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8">
 
                 <dt class="text-sm font-medium leading-6 text-gray-500">
-                  Places restantes
+                   Remaining seats
                 </dt>
 
                 <dd class="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">
@@ -128,7 +128,9 @@ const redirectBooking = (flight: Flight) => {
               </button>
               </div>
           </div>
-          <div v-else>complet</div>
+          <div v-else>
+            Full
+          </div>
         </div>
       </div>
     </div>
