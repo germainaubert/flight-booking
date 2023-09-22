13ae9d7311dea1d53a0de960cefed6b8f0c3afc0
gaubert <germain40k@hotmail.fr>
2023-09-22 15:07:12 +0200

    queue

--- queue/src/queueHandler.ts
+++ queue/src/queueHandler.ts
@@ -5,26 +5,35 @@ const queue = new Queue<Booking>();
 const interval = 5000; // == 5 sec
 
 
-setInterval(resolveQueue, interval);
 
 export const addBookingToQueue= (booking: Booking) => {
     queue.enqueue(booking);
 }
 
+resolveQueue();
+
 async function resolveQueue() {
     console.log('resolve');
     if(queue.size() !== 0 && await isExternalHealthy()) {
         while(queue.size()) {
-            const booking: Booking= queue.getLastItemValue();
-            //await callCancelBooking(booking);
+            const booking: Booking = queue.getLastItemValue();
+            if(await callCancelBooking(booking)) {
+                queue.dequeue();
+            }
         }
     }
+    setTimeout(resolveQueue, interval);
 }
 
-// todo
-// async function callCancelBooking(booking: Booking): Promise<true | false> {
-//     const res = await fetch("")
-// }
+async function callCancelBooking(booking: Booking): Promise<true | false> {
+    const res = await fetch('http://127.0.0.1:3003/?id=' + booking.id);
+    if(res.ok) {
+        return true;
+    } else {
+        console.log('Queue is unable to cancel booking n°' + booking.id);
+        return false;
+    }
+}
 
 // get the health of the external api
 async function isExternalHealthy(): Promise<boolean> {
